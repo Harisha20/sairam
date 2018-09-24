a4e1bca3f5d71bb19642479c5f9ad522fb9d6aad
Harisha20 <42763813+Harisha20@users.noreply.github.com>
2018-09-24 13:58:04 +0530

    VowelConsotant.java

---
+++ VowelConsonant.java
@@ -0,0 +1,22 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class VowelConsonant {
+
+    public static void main(String[] args) {
+
+        char ch = '$';
+
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
+            System.out.println("vowel");
+        else if(ch=='$')
+        System.out.println("invalid");
+        else
+            System.out.println("consonant");
+            
+
+    }
+}
