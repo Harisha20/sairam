a7c0fe125e279379187be349dcf21046772d4b56
Harisha20 <42763813+Harisha20@users.noreply.github.com>
2018-09-25 09:52:25 +0530

    Create Natural.java

---
+++ Natural.java
@@ -0,0 +1,24 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Natural 
+{
+    public static void main(String args[])
+    {
+        int x, i = 1 ;
+        int sum = 0;
+        
+        
+        Scanner s = new Scanner(System.in);
+        x = s.nextInt();
+        while(i <= x)
+        {
+            sum = sum +i;
+            i++;
+        }
+        System.out.println(""+sum);
+    } 
+}
