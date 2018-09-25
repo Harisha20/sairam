44ac0999f4fd5a2a1a9f779a41d6644eb09e019d
Harisha20 <42763813+Harisha20@users.noreply.github.com>
2018-09-25 09:37:34 +0530

    Hello.java

---
+++ Hello.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+
+class Hello
+{
+	public static void main (String[] args)
+	{
+		  Scanner sc = new Scanner(System.in); 
+        int N = sc.nextInt(); 
+		for (int i = 0; i < N; i++) {
+			System.out.println("Hello");
+		}
+	}
+}
